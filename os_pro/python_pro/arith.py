731a685c1411592790d5cbb44736fe4d6bf6e869
khushi <kushishetty354@gmail.com>
2023-08-18 15:33:22 +0530

    subracation function added

--- os_pro/python_pro/arith.py
+++ os_pro/python_pro/arith.py
@@ -1,6 +1,9 @@
 def add(n1,n2):
     return(n1+n2)
+def subraction(n1,n2):
+    return(n1-n2)
 
 a=int(input("enter num1:"))
 b=int(input("enter num2:"))
 print("sum=",add(a,b))
+print("difference=",subraction(a,b))
