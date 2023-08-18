b7e853c923edfd1e8d996be1f873fb707722fa20
khushi <kushishetty354@gmail.com>
2023-08-18 15:17:20 +0530

    multiplication function added

--- os_pro/python_pro/arith.py
+++ os_pro/python_pro/arith.py
@@ -1,6 +1,10 @@
 def add(n1,n2):
     return(n1+n2)
 
+def multiplication(n1,n2):
+    return(n1*n2)
+
 a=int(input("enter num1:"))
 b=int(input("enter num2:"))
 print("sum=",add(a,b))
+print("product=",multiplication(a,b))
